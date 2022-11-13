bd9b41bcf17ce0b0dd111dc7574002474292e206
Rafi030615 <117267872+Rafi030615@users.noreply.github.com>
2022-11-13 21:24:58 +0700

    Update Tugas_Hadiah_MBC1.py

--- Tugas_TKInter/Tugas_Hadiah_MBC1.py
+++ Tugas_TKInter/Tugas_Hadiah_MBC1.py
@@ -40,3 +40,50 @@ def delTodo():
     selectedItem = treev.focus()
     todos[tanggal].pop(treev.item(selectedItem)['text'])
     ListTodo()
+    
+def ListTodo(cb = None):
+    for i in treev.get_children():
+        treev.delete(i)
+    tanggal = str(cal.selection_get())
+    if tanggal in todos:
+        for i in range(len(todos[tanggal])):
+            treev.insert("","end", text = i, values= (todos[tanggal][i]['Waktu'], todos[tanggal][i]['Judul']))
+    
+def addTodo(win, key, jam, menit, judul, keterangan):
+    newTodo = {
+    "Waktu":"{}:{}".format(jam.get(), menit.get()),
+    "Judul":judul.get(),
+    "Keterangan": keterangan.get("1.0", tk.END)
+    }
+    if key in todos:
+        todos[key].append(newTodo)
+    else:
+        todos[key] = [newTodo]
+    win.destroy()
+    ListTodo()
+
+def AddForm():
+    win = tk.Toplevel()
+    win.wm_title("+")
+    jam = tk.IntVar(value = 10)
+    menit = tk.IntVar(value = 30)
+    judul = tk.StringVar(value = "")
+    tk.Label(win, text= "waktu").grid(row=0, column=0)
+    tk.Spinbox(win, from_= 0, to= 23, textvariable= jam, width=3).grid(row=0, column =1)
+    tk.Spinbox(win, from_= 0, to= 59, textvariable= menit, width=3).grid(row=0, column =2)
+    tk.Label(win, text="Judul:").grid(row=1, column =0)
+    tk.Entry(win, textvariable= judul).grid(row = 1, column=1, columnspan=2)
+    tk.Label(win, text="keterangan:").grid(row=2, column=0)
+    keterangan = ScrolledText(win, width = 20, height = 5)
+    keterangan.grid(row=2, column=1, columnspan=2, rowspan=4)
+    tanggal = str(cal.selection_get())
+    tk.Button(win, text = "tambah", command = lambda: addTodo(win, tanggal, jam, menit, judul, keterangan)).grid(row = 6, columnspan = 3)
+
+def title():
+    waktu = strftime("%H : %M")
+    tanggal_now = strftime("%Y-%m-%d")
+    #Saya menambahkan variabel tanggal_now yang berisikan tanggal secara realtime
+    tanggal = str(cal.selection_get())
+    root.title(tanggal_now + " | " + waktu + " | Tanggal yang dipilih : " + tanggal)
+    #Saya menambahkan tanggal_now ke root.title supaya menampilkan tanggal secara realtime
+    root.after(1000, title)
